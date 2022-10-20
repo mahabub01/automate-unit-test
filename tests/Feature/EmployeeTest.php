d310ab0b9ca1e0299dd2c5d79262407c2557e62b
mehrab <mehrab.hossain@isocial.com.bd>
2022-10-20 18:35:50 +0600

    make employee Test

---
+++ tests/Feature/EmployeeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class EmployeeTest extends \Tests\TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function show_the_list_of_employee(): void{
+        $condit = [];
+        $this->assertIsArray($condit);
+    }
+
+}
